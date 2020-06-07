9c6d71d8a91a7377019f6d8e70a0c8fc422170c8
Kosekos <sinkov.ks@gmail.com>
2020-06-07 13:00:09 +0300

    Это наш первы коммит

---
+++ script_App.js
@@ -0,0 +1,23 @@
+let profit = +prompt("Ваш бюджет на месяц?", ""),
+    date   = prompt("Введите дату в формате YYYY-MM-DD", "");
+    
+let appData = {
+    budget: profit,
+    timeData: date,
+    expanses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let exp1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    exp2 = +prompt("Во сколько обойдется?", ""),
+    exp3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    exp4 = +prompt("Во сколько обойдется?", "");
+
+appData.expanses[exp1] = exp2;
+appData.expanses[exp2] = exp3;
+
+alert('Бюджет на один день =' + profit/30);
+
+
