a1292b2afd819466c431f6d49693c50fd7fc25bc
Kosekos <sinkov.ks@gmail.com>
2020-06-07 22:23:23 +0300

    Добавлены проверки на вводимые пользователем значения

--- script_App.js
+++ script_App.js
@@ -22,14 +22,36 @@ for (let i = 0; i < 2; i ++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
         b = +prompt("Во сколько обойдется?", "");
 
-    appData.expanses[a] = b;
+    //Проверка или а == строка,  
+    //(typeof(a)) != null -- пользователь не нажал "отмена"
+    // a != '' -- не пустая строка
+
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) {
+        console.log('done');
+    
+        appData.expanses[a] = b;
+    } else {
+
+        // необходимо написать код, чтобы вернуться к вопросу заново!
+
+    }
 }
 
 // appData.expanses[exp1] = exp2;
 // appData.expanses[exp2] = exp3;
 
+appData.moneyPerDay = appData.budget / 30;
 
+alert('Бюджет на один день: ' + appData.moneyPerDay);
 
-alert('Бюджет на один день =' + profit/30);
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log('Произошла какая-то ошибка');
+}
 
 
