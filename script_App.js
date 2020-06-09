a4d1f50088bd6aa7dd2a7d6bcbfaa2463b486081
Kosekos <sinkov.ks@gmail.com>
2020-06-09 11:24:40 +0300

    Подготовка ко второму ДЗ

--- script_App.js
+++ script_App.js
@@ -11,14 +11,46 @@ let appData = {
 };
 
 // Добавляем значения в переменные
-let exp1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    exp2 = +prompt("Во сколько обойдется?", ""),
-    exp3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    exp4 = +prompt("Во сколько обойдется?", "");
+// let exp1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     exp2 = +prompt("Во сколько обойдется?", ""),
+//     exp3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     exp4 = +prompt("Во сколько обойдется?", "");
 
-appData.expanses[exp1] = exp2;
-appData.expanses[exp2] = exp3;
+// Добавляем значения в переменные с помощью массива
 
-alert('Бюджет на один день =' + profit/30);
+for (let i = 0; i < 2; i ++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = +prompt("Во сколько обойдется?", "");
+
+    //Проверка или а == строка,  
+    //(typeof(a)) != null -- пользователь не нажал "отмена"
+    // a != '' -- не пустая строка
+
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) {
+        console.log('done');
+    
+        appData.expanses[a] = b;
+    } else {
+        // необходимо написать код, чтобы вернуться к вопросу заново!
+    }
+}
+
+// appData.expanses[exp1] = exp2;
+// appData.expanses[exp2] = exp3;
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Бюджет на один день: ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log('Произошла какая-то ошибка');
+}
+
 
 
